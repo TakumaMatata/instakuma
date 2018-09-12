66d46f6eb3a4e0ad703a57813661ebf20b02b1eb
takumanaruke <leon32taku@gmail.com>
2018-09-12 11:21:10 +0900

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, length: {in: 6..20}, on: :create
+
+  # t.string :email
+  # t.string :password
+  # t.boolean :admin
 end
