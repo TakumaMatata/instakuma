58a0ce266479990ecea17c48c307ee87d3b6b62f
takumanaruke <leon32taku@gmail.com>
2018-09-15 13:52:34 +0900

    seeds

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Post.destroy_all
+User.destroy_all
+
+puts 'Creating posts...'
+posts_attributes = [
+  {
+    title: Faker::RuPaul.queen ,
+    post_date: Faker::Date.between(5.days.ago, Date.today) ,
+    description: Faker::Friends.quote ,
+  },
+
+]
+
+takuma = User.new(email: "takuma@gmail.com", password: "123456")
+
+
+takuma.save!
+Post.create!(posts_attributes)
+puts 'Finished!'
+
+
+# t.string "title"
+#     t.date "post_date"
+#     t.text "description"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.string "photo"
